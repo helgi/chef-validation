ea4887526bfc4091f4d6b0f6db628bd3a7f78384
helgi <helgith@gmail.com>
2015-04-30 09:00:16 -0700

    type can't be array and validated against an array of strings as the previous examples expects. Utilise the plural  even if Chef metadata doesn't inherently know about it

--- lib/chef/validation/validator.rb
+++ lib/chef/validation/validator.rb
@@ -67,6 +67,9 @@ module Chef::Validation
         if rules["choice"].present?
           errors += validate_choice(value, rules["choice"], name)
         end
+        if rules["choices"].present?
+          errors += validate_choice(value, rules["choices"], name)
+        end
 
         errors
       end
